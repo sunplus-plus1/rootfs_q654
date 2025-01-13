5c7ea15a79ace2628d5bc03baa79110a6db9266f
jacky.hsieh <jacky.hsieh@sunplus.com>
2025-01-13 16:29:00 +0800

    1. Modify space indent to tab 2. Create upper work folder of overlay in ubifs

--- initramfs/buildroot/systemd/usr/bin/resize_partition.sh
+++ initramfs/buildroot/systemd/usr/bin/resize_partition.sh
@@ -5,42 +5,42 @@
 ## EMMC   : DEVPART=10 => overlay partition
 
 if [ "$DEVTYPE" = "EMMC" ]; then
-  DEVICE=/dev/mmcblk0
+	DEVICE=/dev/mmcblk0
 elif [ "$DEVTYPE" = "SDCARD" ]; then
-  DEVICE=/dev/mmcblk1
+	DEVICE=/dev/mmcblk1
 else
-  echo "Device $DEVTYPE not supported!"
-  exit 1 	
+	echo "Device $DEVTYPE not supported!"
+	exit 1 	
 fi
 
 PARTITION=${DEVICE}p${DEVPART}
 
 # Ensure the partition exists
 if [ ! -b "$PARTITION" ]; then
-    echo "Partition $PARTITION not found!"
-    exit 1
+	echo "Partition $PARTITION not found!"
+	exit 1
 fi
 
 # Resize the partition using parted
 if [ "$DEVTYPE" = "SDCARD" ]; then
-  parted $DEVICE resizepart 2 100%
-  if [ "$?" != "0" ]; then
-    echo "parted failed!"
-    exit 1
-  fi
+	parted $DEVICE resizepart 2 100%
+	if [ "$?" != "0" ]; then
+		echo "parted failed!"
+		exit 1
+	fi
 fi
 
 # Resize the filesystem
 resize2fs $PARTITION
 
 if [ "$?" != "0" ]; then
-  echo "resize2fs failed!"
-  exit 1
+	echo "resize2fs failed!"
+	exit 1
 fi
 
 MULTI_USER_TARGET_WANTS=/etc/systemd/system/multi-user.target.wants
 if [ -L "$MULTI_USER_TARGET_WANTS/resize_partition.service" ]; then
-  rm $MULTI_USER_TARGET_WANTS/resize_partition.service
+	rm $MULTI_USER_TARGET_WANTS/resize_partition.service
 fi
 
 echo "Partition $PARTITION resized to maximum capacity."
